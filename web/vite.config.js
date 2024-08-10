6b14f6c345f58de8c7cfa99b6351aaeb694f611f
tkuramot <mabdman512@gmail.com>
2024-08-11 02:14:50 +0900

    add dockerfile

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,12 +2,13 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  publicDir: "../public",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
-  root: path.resolve(__dirname, "./src"),
+  server: {
+    host: "0.0.0.0",
+  },
 });
