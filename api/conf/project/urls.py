d736ed40a4cd6dfcea56d9365e3b458dd9dcd202
sota <hotphilly1776@yahoo.co.jp>
2024-08-21 10:01:51 +0900

    change path to admin

--- api/conf/project/urls.py
+++ api/conf/project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/plyr/', include('plyr.urls')),
     path('api/tmt/', include('tmt.urls')),
     path('api/mtch/', include('mtch.urls')),
