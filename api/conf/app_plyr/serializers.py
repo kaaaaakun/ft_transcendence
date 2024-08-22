ac30748fb255a2f00057d5ce71174bbfc54d6b82
sota <hotphilly1776@yahoo.co.jp>
2024-08-22 22:40:45 +0900

    Divide player utils function into validate and register.

--- api/conf/app_plyr/serializers.py
+++ api/conf/app_plyr/serializers.py
@@ -7,10 +7,10 @@ class PlayerSerializer(serializers.ModelSerializer):
 		fields = '__all__'
 
 	# 受け取ったdataが、モデルのフィールドに適合するか検証する。
-	def validate(self, data)
-		if 'name' not in data 
+	def validate(self, data):
+		if 'name' not in data:
 			raise serializers.ValidationError("key 'name' is required.")
-		if data['name'].strip():
-			raise serializers.ValidationError("date 'Name' cannot be blank or only spaces.")
+		if not data['name'].strip():
+			raise serializers.ValidationError("data 'Name' cannot be blank or only spaces.")
 		return data
 	
--- api/conf/app_plyr/utils.py
+++ api/conf/app_plyr/utils.py
@@ -2,18 +2,26 @@ from .models import Player
 from .serializers import PlayerSerializer
 from rest_framework.exceptions import ValidationError
 
-# 複数のプレイヤーをDBに登録する
+# Validate multiple player
 # args: 文字列の配列
-# return: Playerオブジェクトのリスト
-# serializerでvalidationし、DBに登録する。
-def register_players(player_names):
-	players = []
+# return: Playerインスタンスのリスト
+def validate_players(player_names):
+	valid_players_data = []
 	for name in player_names:
-		player_data = {"name", name}
+		player_data = {"name": name}
 		player_serializer = PlayerSerializer(data=player_data)
 		if player_serializer.is_valid(raise_exception = True):
-			player = player_serializer.save() # save to DB
-			players.append(player)
+			valid_players_data.append(player_serializer.validated_data)
 		else:
 			raise ValidationError(player_serializer.errors)
+	return valid_players_data
+
+# 複数のプレイヤーをDBに登録する
+# args: Playerインスタンス(validated)のリスト
+# return: Playerオブジェクトのリスト
+def register_players(valid_players_data):
+	players = []
+	for player_data in valid_players_data:
+			player = Player.objects.create(**player_data)
+			players.append(player)
 	return players
