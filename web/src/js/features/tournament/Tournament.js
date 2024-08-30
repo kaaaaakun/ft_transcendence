93dce3d51c5bc2a5c30cf5ad7fce0a134fcf6def
sinagaki <soma0822soma@gmail.com>
2024-08-30 23:00:01 +0900

    rename .shift-up-200

--- web/src/js/features/tournament/Tournament.js
+++ web/src/js/features/tournament/Tournament.js
@@ -45,7 +45,7 @@ function Tournament({ players }) {
   }
   return Teact.createElement(
       'div',
-      { className: 'position-relative mt-200'},
+      { className: 'position-relative　shift-up-200'},
       Teact.createElement(
         'svg',
         {
@@ -203,44 +203,5 @@ function Tournament({ players }) {
       )
     );
 }
-
-const players = {
-  1: { name: "Alice", victoryCount: 1, nextPlayer: false },
-  2: { name: "Bob", victoryCount: 0, nextPlayer: false },
-  3: { name: "Charlie", victoryCount: 2, nextPlayer: true },
-  4: { name: "David", victoryCount: 0, nextPlayer: false },
-  5: { name: "Eve", victoryCount: 1, nextPlayer: false },
-  6: { name: "Frank", victoryCount: 0, nextPlayer: false },
-  7: { name: "Grace", victoryCount: 2, nextPlayer: true },
-  8: { name: "Hank", victoryCount: 0, nextPlayer: false }
-};
-
-// fetch('http://127.0.0.1:8000/api/plyr/data/')
-//   .then(response => {
-//     // レスポンスが成功かどうかを確認
-//     if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//     }
-//     // レスポンスをJSON形式に変換
-//     return response.json();
-//   })
-//   .then(data => {
-//     // 取得したJSONデータを利用
-//     console.log(data);
-//     const players = data.reduce((acc, item) => {
-//       const key = Object.keys(item)[0];  // キーを取得
-//       acc[key] = item[key];  // オブジェクトにキーと値を追加
-//       return acc;
-//     }, {});
-//     const container = document.getElementById('app');
-//     Teact.render(Teact.createElement(App, { players }), container);
-//     // ここでデータの処理を行う（例: DOMに表示するなど）
-//   })
-//   .catch(error => {
-//     // エラー処理
-//     console.error('There was a problem with the fetch operation:', error);
-// });
-
-
 
 export { Tournament }
