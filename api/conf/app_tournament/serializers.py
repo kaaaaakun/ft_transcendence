2b8f42f31991769483569cf2d697cda3a4764aea
sinagaki <soma0822soma@gmail.com>
2024-08-31 16:07:38 +0900

    TournamentPlayerSerializer validate

--- api/conf/app_tournament/serializers.py
+++ api/conf/app_tournament/serializers.py
@@ -27,3 +27,27 @@ class TournamentPlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = TournamentPlayer
         fields = '__all__'
+
+    def validate(self, data):
+        if 'tournament_id' not in data:
+            raise serializers.ValidationError("key 'tournament_id' is required.")
+        if 'player_id' not in data:
+            raise serializers.ValidationError("key 'player_id' is required.")
+        if 'status' not in data:
+            raise serializers.ValidationError("key 'status' is required.")
+        if 'victory_count' not in data:
+            raise serializers.ValidationError("key 'victory_count' is required.")
+        if data['victory_count'] != 0:
+            raise serializers.ValidationError("TournamentPlayer 'victory_count' must be 0.")
+        if data['status'] != 'await':
+            raise serializers.ValidationError("TournamentPlayer 'status' must be 'await'.")
+        
+        tournament = Tournament.objects.get(id = data['tournament_id'])
+        if tournament.status == 'end':
+            raise serializers.ValidationError("Cannot create a tournamentplayer for a tournament that has already ended.")
+        
+        players = TournamentPlayer.objects.filter(tournament_id = data['tournament_id'])
+        if tournament.num_of_player <= players.count():
+            raise serializers.ValidationError("Cannot create a tournamentplayer for a tournament that has already full.")
+        
+        return data
