5192a7665d17eb65a56cccaab0f7333506b1d782
soma0822 <120237021+soma0822@users.noreply.github.com>
2024-09-04 06:13:21 +0900

    Api businesslogic stanby image (#31)
    * TournamentPlayerSerializer validate
    
    * create_tournamente
    
    * create post request
    
    * create test
    
    * create post test
    
    * use APITestCase
    
    * fix id -> player_id
    
    * Merge branch 'main' into Api_businesslogic_stanby_image
    
    * Merge branch 'main' into Api_businesslogic_stanby_image
    
    * fix test

--- api/conf/app_tournament/serializers.py
+++ api/conf/app_tournament/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Tournament, TournamentPlayer
 
-STATUS_CHOICES = ['start', 'end']
-MIN_PLAYERS = 1
-MAX_PLAYERS = 8
+TOURNAMENT_STATUS_CHOICES = ['start', 'end']
+TOURNAMENT_PLAYER_STATUS_CHOICES = ['start', 'end', 'await']
+NUM_OF_PLAYER_CHOICES = [2, 4, 8]
 
 class TournamentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,15 +15,33 @@ class TournamentSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("key 'status' is required.")
         if 'num_of_player' not in data:
             raise serializers.ValidationError("key 'num_of_player' is required.")
-        if data['status'] not in STATUS_CHOICES:
+        if data['status'] not in TOURNAMENT_STATUS_CHOICES:
             raise serializers.ValidationError("Tournament 'status' is invalid value.")
-        if data['num_of_player'] < MIN_PLAYERS or MAX_PLAYERS < data['num_of_player']:
+        if data['num_of_player'] not in NUM_OF_PLAYER_CHOICES:
             raise serializers.ValidationError(f"Tournament 'num_of_player':{data['num_of_player']} is invalid.")
-        if (data['num_of_player'] & (data['num_of_player'] - 1)) != 0: #https://programming-place.net/ppp/contents/c/rev_res/math012.html#way1
-            raise serializers.ValidationError(f"Tournament 'num_of_player':{data['num_of_player']} must be a power of 2.")
         return data
 
 class TournamentPlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = TournamentPlayer
         fields = '__all__'
+
+    def validate(self, data):
+        if 'tournament_id' not in data:
+            raise serializers.ValidationError("key 'tournament_id' is required.")
+        if 'player_id' not in data:
+            raise serializers.ValidationError("key 'player_id' is required.")
+        if 'status' not in data:
+            raise serializers.ValidationError("key 'status' is required.")
+        if 'victory_count' not in data:
+            raise serializers.ValidationError("key 'victory_count' is required.")
+        if data['status'] not in TOURNAMENT_PLAYER_STATUS_CHOICES:
+            raise serializers.ValidationError("TournamentPlayer 'status' is invalid value.")
+        if data['victory_count'] < 0:
+            raise serializers.ValidationError("TournamentPlayer 'victory_count' must be a non-negative integer.")
+        
+        tournament = Tournament.objects.get(id = data['tournament_id'].id)
+        if tournament.status == 'end':
+            raise serializers.ValidationError("Cannot create a tournamentplayer for a tournament that has already ended.")
+        
+        return data
