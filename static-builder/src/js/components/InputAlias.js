3ab7c551b6f9cbcbc763ee56c108f16470ae73bd
sinagaki <soma0822soma@gmail.com>
2024-09-10 19:56:59 +0900

    fix InputAlias.js

--- static-builder/src/js/components/InputAlias.js
+++ static-builder/src/js/components/InputAlias.js
@@ -14,36 +14,18 @@ function handleSubmit(event) {
   })
 }
 
-function App() {
-  const [numberOfPlayers, setNumberOfPlayers] = Teact.useState(2)
-
+export function InputAlias(num) {
   return BaseLayout(
     Teact.createElement(
       'div',
       { className: 'container' },
-      Teact.createElement(
-        'div',
-        { className: 'd-flex justify-content-center' },
-        DefaultButton({
-          text: '2 Players',
-          onClick: () => setNumberOfPlayers(c => 2),
-        }),
-        DefaultButton({
-          text: '4 Players',
-          onClick: () => setNumberOfPlayers(c => 4),
-        }),
-        DefaultButton({
-          text: '8 Players',
-          onClick: () => setNumberOfPlayers(c => 8),
-        }),
-      ),
       Teact.createElement(
         'form',
         {
           onSubmit: handleSubmit,
           className: 'text-center mt-3 d-grid gap-2 col-3 mx-auto',
         },
-        ...Array.from({ length: 4 }, (_, i) => {
+        ...Array.from({ length: num / 2 }, (_, i) => {
           const className =
             i >= numberOfPlayers / 2
               ? 'form-control mt-2 bg-secondary'
@@ -59,7 +41,6 @@ function App() {
                 className: className,
                 placeholder: `Player ${i * 2 + 1}`,
                 name: `player${i * 2}`,
-                disabled: i >= numberOfPlayers / 2,
               }),
             ),
             Teact.createElement(
@@ -70,7 +51,6 @@ function App() {
                 className: className,
                 placeholder: `Player ${i * 2 + 2}`,
                 name: `player${i * 2 + 1}`,
-                disabled: i >= numberOfPlayers / 2,
               }),
             ),
           )
@@ -82,4 +62,4 @@ function App() {
 }
 
 const container = document.getElementById('app')
-Teact.render(Teact.createElement(App), container)
+Teact.render(Teact.createElement(InputAlias(2)), container)
