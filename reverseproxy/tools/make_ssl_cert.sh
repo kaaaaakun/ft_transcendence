1c38a83edbb334f2dc277b56494eee29d6f8c43c
kaaaaakun_ <135812521+kaaaaakun@users.noreply.github.com>
2024-09-19 00:32:22 +0900

    fix reverseproxy/tools/make_ssl_cert.sh

--- reverseproxy/tools/make_ssl_cert.sh
+++ reverseproxy/tools/make_ssl_cert.sh
@@ -2,16 +2,24 @@
 
 # 署名証明書を生成するシェルスクリプト
 
+# 環境変数をロード
 source ../../.env
 
-mkdir -p ../ssl
+# 出力ディレクトリ
+SSL_DIR="../ssl"
+KEY_FILE="$SSL_DIR/ft_transcendence.key"
+CSR_FILE="$SSL_DIR/ft_transcendence.csr"
+CRT_FILE="$SSL_DIR/ft_transcendence.crt"
 
-# 鍵ペアを生成
-openssl genpkey -algorithm RSA -out ../ssl/inception.key
+# SSL ディレクトリが存在しない場合は作成
+mkdir -p "$SSL_DIR"
 
-# 証明書署名要求を生成
-openssl req -new -key ../ssl/inception.key -out ../ssl/inception.csr \
-	-subj "/C=${CSR_COUNTRY}/ST=${CSR_STATE}/L=${CSR_LOCALITY}/O=${CSR_ORGANIZATION}/OU=${CSR_ORGANIZATIONAL_UNIT}/CN=${CSR_COMMON_NAME}"
+# 鍵ペアを生成 (RSA アルゴリズム)
+openssl genpkey -algorithm RSA -out "$KEY_FILE"
 
-# 証明書を署名
-openssl x509 -req -days 365 -in ../ssl/inception.csr -signkey ../ssl/inception.key -out ../ssl/inception.crt
+# 証明書署名要求 (CSR) を生成
+openssl req -new -key "$KEY_FILE" -out "$CSR_FILE" \
+  -subj "/C=${CSR_COUNTRY}/ST=${CSR_STATE}/L=${CSR_LOCALITY}/O=${CSR_ORGANIZATION}/OU=${CSR_ORGANIZATIONAL_UNIT}/CN=${CSR_COMMON_NAME}"
+
+# CSR を使用して自己署名証明書を生成 (有効期限365日)
+openssl x509 -req -days 365 -in "$CSR_FILE" -signkey "$KEY_FILE" -out "$CRT_FILE"
