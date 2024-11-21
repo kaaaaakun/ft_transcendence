34b467810a4017d201b699ab34a0156613337531
sota <hotphilly1776@yahoo.co.jp>
2024-11-21 12:04:13 +0900

    [wip]開発環境のセッティング。ws通信デモをしてみたら設定ミスがあったので修正。

--- api/conf/project/asgi.py
+++ api/conf/project/asgi.py
@@ -11,11 +11,12 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from match.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trans.settings')
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": URLRouter(websocket_urlpatterns),
 })
