fb826809a5d26721c6d696e6ae08ef4bb11eb50a
sota <hotphilly1776@yahoo.co.jp>
2024-12-08 11:14:49 +0900

    fix: update WebSocket routes for local match consumers to improve clarity related to AsyncAPIの修正 #151

--- api/conf/app_match/routing.py
+++ api/conf/app_match/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^api/ws/smatch/$', consumers.LocalSimpleMatchConsumer.as_asgi()),
-    re_path(r'^api/ws/tmatch/$', consumers.LocalTournamentMatchConsumer.as_asgi()),
+    re_path(r'^api/ws/local-simple-match/$', consumers.LocalSimpleMatchConsumer.as_asgi()),
+    re_path(r'^api/ws/local-tournament-match/$', consumers.LocalTournamentMatchConsumer.as_asgi()),
 ]
