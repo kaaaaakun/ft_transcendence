20974cf0e93fe692e4488040a4e704cc280bdc13
kaaaaakun <135812521+kaaaaakun@users.noreply.github.com>
2025-01-09 00:07:59 +0900

    wip 一旦push

--- api/conf/project/settings.py
+++ api/conf/project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ft_transcendence.42.com']
+# 一時的に変更ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ft_transcendence.42.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
