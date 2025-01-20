2d77a53504a43aefc5093dec77c3b41306959d32
Emu Kamada <es377233@gmail.com>
2025-01-20 21:30:05 +0900

    model定義に合わせる

--- api/conf/app_user/models.py
+++ api/conf/app_user/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 class User(models.Model):
-    login_name = models.CharField(max_length=20)
-    password = models.CharField(max_length=255)
-    display_name = models.CharField(max_length=20)
+    id = models.AutoField(primary_key=True)
+    login_name = models.CharField(max_length=20, unique=True)
+    display_name = models.CharField(max_length=20, unique=True)
+    avatar_path = models.CharField(max_length=255, default=None, null=True)
+    password_hash = models.CharField(max_length=255)
     secret_question = models.CharField(max_length=255)
-    secret_answer = models.CharField(max_length=255)
+    secret_answer_hash = models.CharField(max_length=255)
+    last_online_at = models.DateTimeField(default=None, null=True)
     deleted_at = models.DateTimeField(default=None, null=True)
 
 
