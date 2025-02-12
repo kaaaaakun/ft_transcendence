2ae34248a1adc9738e23b009cba275b29b4e7eec
tkuramot <106866329+tkuramot@users.noreply.github.com>
2025-02-12 12:25:23 +0900

    pre-commit hook を導入する (#191)
    * pre-push hook を導入する
    
    * format check
    
    * lefthook.yml を修正する
    
    * lint も追加する
    
    * フォーマットが失敗したときにエラーにしつつフォーマットする
    
    * lint & format

--- static-builder/src/js/main.js
+++ static-builder/src/js/main.js
@@ -1,72 +1,73 @@
-import "@/scss/styles.scss";
-import { Route, Router } from "@/js/libs/router";
-import { Teact } from "@/js/libs/teact";
-import { FriendsList } from "@/js/pages/FriendsList";
-import { Home } from "@/js/pages/Home";
-import { TournamentsDisplayName } from "@/js/pages/TournamentsDisplayName";
-import { SimpleGameLocal } from "@/js/pages/SimpleGameLocal";
-import { Login } from "@/js/pages/Login";
-import { TournamentsGame } from "@/js/pages/TournamentsGame";
-import { Register } from "@/js/pages/Register";
-import { TournamentsIndex } from "@/js/pages/TournamentsIndex";
-import { TournamentsBracket } from "@/js/pages/TournamentsBracket";
-import { DeleteAccount } from "@/js/pages/DeleteAccount";
-import { PasswordReset } from "@/js/pages/PasswordReset";
+import '@/scss/styles.scss'
+import { Route, Router } from '@/js/libs/router'
+import { Teact } from '@/js/libs/teact'
+import { FriendsList } from '@/js/pages/FriendsList'
+import { Home } from '@/js/pages/Home'
+import { Login } from '@/js/pages/Login'
+import { Register } from '@/js/pages/Register'
+import '@/scss/styles.scss'
+import { TournamentsDisplayName } from '@/js/pages/TournamentsDisplayName'
+import { SimpleGameLocal } from '@/js/pages/SimpleGameLocal'
+import { TournamentsGame } from '@/js/pages/TournamentsGame'
+import { TournamentsIndex } from '@/js/pages/TournamentsIndex'
+import { TournamentsBracket } from '@/js/pages/TournamentsBracket'
+import { DeleteAccount } from '@/js/pages/DeleteAccount'
+import { PasswordReset } from '@/js/pages/PasswordReset'
 
 function App() {
   return Router(
-    Route({ path: "/", component: Home }),
+    Route({ path: '/', component: Home }),
     Route({
-      path: "/about",
-      component: Teact.createElement("h1", null, "About"),
+      path: '/about',
+      component: Teact.createElement('h1', null, 'About'),
     }),
     Route({
-      path: "/contact",
-      component: Teact.createElement("h1", null, "Contact"),
+      path: '/contact',
+      component: Teact.createElement('h1', null, 'Contact'),
     }),
     Route({
-      path: "/simple-game/local",
+      path: '/simple-game/local',
       component: SimpleGameLocal,
     }),
     Route({
-      path: "/tournaments",
+      path: '/tournaments',
       component: TournamentsIndex,
     }),
     Route({
-      path: "/tournaments/display-name",
+      path: '/tournaments/display-name',
       component: TournamentsDisplayName,
     }),
     Route({
-      path: "/tournaments/bracket",
+      path: '/tournaments/bracket',
       component: TournamentsBracket,
     }),
     Route({
-      path: "/tournaments/game",
+      path: '/tournaments/game',
       component: TournamentsGame,
     }),
     Route({
-      path: "/users/:id/friends",
+      path: '/users/:id/friends',
       component: FriendsList,
     }),
     Route({
-      path: "/login",
+      path: '/login',
       component: Login,
     }),
     Route({
-      path: "/register",
+      path: '/register',
       component: Register,
     }),
     Route({
-      path: "/password-reset",
+      path: '/password-reset',
       component: PasswordReset,
     }),
     Route({
-      path: "/delete-account",
+      path: '/delete-account',
       component: DeleteAccount,
     }),
-  );
+  )
 }
 
-const element = Teact.createElement(App);
-const container = document.getElementById("app");
-Teact.render(element, container);
+const element = Teact.createElement(App)
+const container = document.getElementById('app')
+Teact.render(element, container)
