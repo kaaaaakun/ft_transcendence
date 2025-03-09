a05e642a1359db11419a91bbc34dc72c6e46ec71
soragogo <es377233@gmail.com>
2025-03-09 20:52:51 +0000

    シリアライザーの導入

--- api/conf/app_user/views.py
+++ api/conf/app_user/views.py
@@ -1,4 +1,5 @@
 import random
+from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.views import APIView
 from django.views import View
@@ -10,73 +11,39 @@ import json
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
+from .serializers import (
+    UserSerializer,
+    UserLoginSerializer,
+    PasswordResetRequestSerializer,
+    PasswordResetSerializer
+)
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class UserLoginView(APIView):
     def post(self, request, *args, **kwargs):
-        try:
-            data = json.loads(request.body)
-            login_name = data.get('login_name')
-            password = data.get('password')
-
-            if not login_name or not password:
-                return JsonResponse({
-                    'error': 'Login name and password are required.'
-                }, status=status.HTTP_400_BAD_REQUEST)
-
-            user = User.ft_authenticate(login_name=login_name, password=password)
-
-            if user is not None:
-                refresh = RefreshToken.for_user(user)
-                return JsonResponse({
-                    'message': 'Login successful',
-                    'access_token': str(refresh.access_token),
-                    'refresh_token': str(refresh),
-                }, status=status.HTTP_200_OK)
-            else:
-                return JsonResponse({
-                    'error': str('Login failed. Check your login name and password.')
-                }, status=status.HTTP_400_BAD_REQUEST)
-
-        except Exception as e:
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
             return JsonResponse({
-                'error': 'Something went wrong.'
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+                'message': 'Login successful',
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh),
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class UserView(APIView):
+    authentication_classes = [JWTAuthentication]
+
     def post(self, request, *args, **kwargs):
-        try:
-            data = json.loads(request.body)
-
-            login_name = data.get('login_name')
-            display_name = data.get('display_name')
-            password = data.get('password')
-            secret_question = data.get('secret_question')
-            secret_answer = data.get('secret_answer')
-
-            if not login_name or not password or not display_name or not secret_question or not secret_answer:
-                return JsonResponse({
-                    'error': 'All fields are required.',
-                    'Your request': str(data)
-                }, status=status.HTTP_400_BAD_REQUEST)
-
-            user = User.objects.create_user(
-                login_name=login_name,
-                password=password,
-                display_name=display_name,
-                secret_question=secret_question,
-                secret_answer=secret_answer
-            )
-
-
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return JsonResponse({
                 'message': 'Sign up successful',
             }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        except Exception as e:
-            return JsonResponse({
-                'error': 'Something went wrong.',
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def delete(self, request, *args, **kwargs):
         try:
@@ -119,61 +86,50 @@ class UserView(APIView):
             return JsonResponse({
                 'error': 'User not found.'
             }, status=status.HTTP_404_NOT_FOUND)
-            
+
         except Exception as e:
 
             return JsonResponse({
                 'error': 'Something went wrong.'
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 
 class UserPasswordResetView(APIView):
     def get(self, request, login_name,*args, **kwargs):
-        try:
+        serializer = PasswordResetRequestSerializer(data={'login_name': login_name})
+        if serializer.is_valid():
+            try:
+                user = User.objects.get(login_name=login_name)
+                if (user.deleted_at is not None):
+                    raise User.DoesNotExist
 
-            if not login_name:
                 return JsonResponse({
-                    'error': 'Login name is required.'
-                }, status=status.HTTP_400_BAD_REQUEST)
+                    'secret_question': user.secret_question
+                }, status=status.HTTP_200_OK)
 
-            user = User.objects.get(login_name=login_name)
+            except User.DoesNotExist:
+                return JsonResponse({
+                    'error': 'User not found.'
+                }, status=status.HTTP_404_NOT_FOUND)
 
-            if (user.deleted_at is not None):
-                raise User.DoesNotExist
-
-            return JsonResponse({
-                'secret_question': user.secret_question
-            }, status=status.HTTP_200_OK)
-
-        except User.DoesNotExist:
-            return JsonResponse({
-                'error': 'User not found.'
-            }, status=status.HTTP_404_NOT_FOUND)
-
-        except Exception as e:
-            return JsonResponse({
-                'error': 'Something went wrong.'
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            except Exception as e:
+                return JsonResponse({
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
     def post(self, request, login_name,*args, **kwargs):
-        try:
-            data = json.loads(request.body)
-            login_name = data.get('login_name')
-            secret_answer = data.get('secret_answer')
-            new_password = data.get('new_password')
+        data = json.loads(request.body)
+        data['login_name'] = login_name
 
-            if not login_name or not secret_answer or not new_password:
-                return JsonResponse({
-                    'error': 'Login name, secret answer and new password are required.'
-                }, status=status.HTTP_400_BAD_REQUEST)
+        serializer = PasswordResetSerializer(data=data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            secret_answer = serializer.validated_data['secret_answer']
+            new_password = serializer.validated_data['new_password']
 
-            user = User.objects.get(login_name=login_name)
-
-            if (user.deleted_at is not None):
-                raise User.DoesNotExist
-
-            if check_password(secret_answer, user.secret_answer_hash):
+            if check_password(secret_answer, user.secret_answer):
                 user.password = make_password(password=new_password)
                 user.save()
                 return JsonResponse({
@@ -183,12 +139,4 @@ class UserPasswordResetView(APIView):
                 return JsonResponse({
                     'error': 'Incorrect secret answer.'
                 }, status=status.HTTP_400_BAD_REQUEST)
-        except User.DoesNotExist:
-            return JsonResponse({
-                'error': 'User not found.'
-            }, status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            return JsonResponse({
-                'error': 'Something went wrong.'
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
