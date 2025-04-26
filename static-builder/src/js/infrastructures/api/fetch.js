b104c25f48d83225108d6086d7174b4e538b4ec9
sinagaki <soma0822soma@gmail.com>
2025-04-26 15:35:31 +0900

    fix: baseurl

--- static-builder/src/js/infrastructures/api/fetch.js
+++ static-builder/src/js/infrastructures/api/fetch.js
@@ -79,4 +79,6 @@ class FetchWrapper {
   }
 }
 
-export const api = new FetchWrapper('http://localhost')
+export const api = new FetchWrapper(
+  import.meta.env.VITE_API_URL ?? 'https://localhost',
+)
