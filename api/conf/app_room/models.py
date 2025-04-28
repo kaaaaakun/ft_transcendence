6b8c659e602f1f69dfff126c10dfb34a5ea83fc9
sota <hotphilly1776@yahoo.co.jp>
2025-04-28 17:49:30 +0900

    Fix RoomMembers model: rename user_id to user and add unique constraint

--- api/conf/app_room/models.py
+++ api/conf/app_room/models.py
@@ -3,7 +3,10 @@ from user.models import User
 
 class RoomMembers(models.Model):
     room_id = models.CharField(max_length = 100)
-    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
 
     class Meta:
         db_table = 'room_members'
+        constraints = [
+            models.UniqueConstraint(fields = ['room_id', 'user'], name = 'unique_room_user')
+        ]
