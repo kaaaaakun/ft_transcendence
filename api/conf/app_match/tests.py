0114df77c4709e7b2ce5ddec0bee0825086624a3
sota <hotphilly1776@yahoo.co.jp>
2025-04-28 18:48:27 +0900

    Add unit tests for Match and Tournament models to validate creation and status updates

--- api/conf/app_match/tests.py
+++ api/conf/app_match/tests.py
@@ -1,24 +1,56 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from user.models import User
 from .models import Match, MatchDetail
-from django.contrib.auth.models import User
+from tournament.tests import create_test_tournament_4, create_test_tournament_8
 
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-'''
-class BaseTestSetup(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Create Users
-        ユーザーモデルでテストケースを作成して、それを参照する形でセットアップ（できるか不明）するほうが、モデルごとのテストケースの記述が少なくてすみそう
+#---------------
+# Helper function to create Data
+#---------------
+def create_test_match_tournament_4():
+    tournament = create_test_tournament_4()
+    return Match.create_for_tournament(tournament)
 
-        # Create simple matches
+def create_test_match_tournament_8():
+    tournament = create_test_tournament_8()
+    return Match.create_for_tournament(tournament)
 
-        # Create matchdetails
+def create_test_match_simple():
+    return Match.objects.create()
 
-        # Create admin user
-        cls.admin = User.objects.create_user(username = 'admin', password = 'pass', is_staff = True)
-'''
+#--------------
+# Test cases for Match model
+#--------------
+class MatchTestCase(TestCase):
+    def test_create_simple_match(self):
+        match = create_test_match_simple()
+        self.assertIsInstance(match, Match)
+        self.assertIsNone(match.tournament)
+
+    def test_create_tournament4_match(self):
+        match = create_test_match_tournament_4()
+        self.assertIsInstance(match, Match)
+
+    def test_create_tournament8_match(self):
+        match = create_test_match_tournament_8()
+        self.assertIsInstance(match, Match)
+
+    def test_is_finished(self):
+        match = create_test_match_tournament_4()
+        match = Match.update_is_finished(match)
+        self.assertTrue(match.is_finished)
+
+    def test_tx_status_unexpected(self):
+        match = create_test_match_tournament_4()
+        with self.assertRaises(ValidationError):
+            Match.update_tx_status(match, 'unexpected_status')
+
+    def test_tx_status_success(self):
+        match = create_test_match_tournament_4()
+        match = Match.update_tx_status(match, 'success')
+        self.assertEqual(match.tx_status, 'success')
--- api/conf/app_tournament/tests.py
+++ api/conf/app_tournament/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 from .models import Tournament, TournamentPlayer
-from player.utils import validate_players, register_players
 from match.models import Match, MatchDetail
 from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
@@ -10,7 +9,31 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+#---------------
+# Helper function to create Data
+#---------------
+def create_test_tournament_4():
+    return Tournament.objects.create(type = 4)
 
-# class BaseTestSetup(TestCase):
+def create_test_tournament_8():  
+    return Tournament.objects.create(type = 8)
 
 
+#--------------
+# Test cases for Tournament model
+#--------------
+class TournamentTestCase(TestCase):
+    def test_create_tournament_4(self):
+        tournament = create_test_tournament_4()
+        self.assertIsInstance(tournament, Tournament)
+        self.assertEqual(tournament.type, 4)
+
+    def test_create_tournament_8(self):
+        tournament = create_test_tournament_8()
+        self.assertIsInstance(tournament, Tournament)
+        self.assertEqual(tournament.type, 8)
+
+    def test_update_status(self):
+        tournament = create_test_tournament_4()
+        Tournament.update_status(tournament)
+        self.assertTrue(tournament.is_finished)
