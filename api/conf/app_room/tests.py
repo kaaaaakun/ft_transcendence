2903ec8d261ac6ff507ebb28e931440b38923f9c
sota <hotphilly1776@yahoo.co.jp>
2025-05-03 14:58:04 +0900

    refactor: streamline RoomMembers test cases and enhance unique constraint validation

--- api/conf/app_room/tests.py
+++ api/conf/app_room/tests.py
@@ -1,21 +1,32 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
 from .models import RoomMembers
-from .utils import RoomKey
-from user.models import User
-
-
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
+from user.tests import create_test_user_4
 
 #---------------
 # Helper function to create Data
 #---------------
-
-
+def create_test_room_members_4():
+    users = create_test_user_4()
+    roomMembers = []
+    for user in users:
+        roomMembers.append(RoomMembers.objects.create(room_id = 'test_room', user = user))
+    return roomMembers
 
 #--------------
-# Test cases for Match model
+# Test cases for RoomMembers model
 #--------------
+class RoomMembersTestCase(TestCase):
+    def test_create_room_members(self):
+        roomMembers = create_test_room_members_4()
+        self.assertEqual(len(roomMembers), 4)
+        for i, roomMember in enumerate(roomMembers):
+            self.assertEqual(roomMember.user.id, i + 1)
+            self.assertEqual(roomMember.room_id, 'test_room')
+
+    def test_unique_constraint(self):
+        users = create_test_user_4()
+        roomMember1 = RoomMembers.objects.create(room_id = 'test_room', user = users[0])
+        with self.assertRaises(IntegrityError):
+            roomMember2 = RoomMembers.objects.create(room_id = 'test_room', user = users[0])
