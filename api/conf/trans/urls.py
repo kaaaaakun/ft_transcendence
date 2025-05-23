01994d93fa26b4f3e7741ba947204cb296a44f4f
sota <hotphilly1776@yahoo.co.jp>
2025-05-23 23:10:11 +0900

    add: create simple match URLs and include in main URL configuration

--- api/conf/trans/urls.py
+++ api/conf/trans/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/tournaments/', include('tournament.urls')),
     path('api/matches/', include('match.urls')),
+    path('api/simple-matches/', include('match.simple_match_urls')),
     path('api/users/', include('user.user_urls')),
     path('api/auth/', include('user.auth_urls')),
     path('api/rooms/', include('room.urls')),
