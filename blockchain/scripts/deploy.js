02e5524d242762dc60e3c81a614e535fe8b65ae1
kaaaaakun <135812521+kaaaaakun@users.noreply.github.com>
2025-05-29 08:08:48 +0900

    fix: deploy成功

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -4,13 +4,14 @@ async function main() {
   const MyContract = await hre.ethers.getContractFactory("MyContract");
   const contract = await MyContract.deploy("Hello, Sepolia!");
 
-  await contract.deployed();
+  await contract.waitForDeployment();
 
-  console.log("Contract deployed to:", contract.address);
+  console.log("Contract deployed to:", await contract.getAddress(), " <- これがSepoliaネットワーク上にデプロイされたコントラクトのアドレス！！");
+  console.log("アドレスを正しいものにして、アクセスしてみよう！\nhttps://sepolia.etherscan.io/address/0x........");
+
 }
 
 main().catch((error) => {
   console.error(error);
   process.exit(1);
 });
-
