35c6d8c7b218867b9f9c0c366c4c88612144d2ce
sota <hotphilly1776@yahoo.co.jp>
2025-05-28 22:01:13 +0900

    Add tests for simple match creation and implement port method for SimpleMatchView

--- api/conf/match/views.py
+++ api/conf/match/views.py
@@ -9,10 +9,11 @@ from .models import Match, MatchDetail
 from .utils import create_simple_match_response
 from user.utils import get_user_by_auth
 from user.exceptions import AuthError
+from room.models import RoomMembers
 from room.utils import RoomKey
 from utils.decorators import admin_only
 
-END_OF_GAME_SCORE = 11
+SIMPLE_WAITING_ROOM_LIMIT = 5
 
 # start: ユースケースでは本来必要ないが、データの確認のために追加
 @method_decorator(admin_only, name = 'dispatch')
@@ -48,3 +49,27 @@ class SimpleMatchView(APIView):
                 return Response(create_simple_match_response(waiting_simple_room_keys), status = status.HTTP_200_OK)
             except AuthError as e:
                 return Response({"error": str(e)}, status = status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Invalid match type."}, status = status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        match_type = request.data.get('type')
+        if match_type != 'remote':
+            return Response({"error": "Invalid match type."}, status = status.HTTP_400_BAD_REQUEST)
+        try:
+            user = get_user_by_auth(request.headers.get('Authorization'))
+            if not user:
+                raise AuthError('Authorization header is incorrect.')
+            waiting_simple_room_keys = RoomKey.get_keys_by_type_and_entry_count("SIMPLE", 2)
+            if len(waiting_simple_room_keys) < SIMPLE_WAITING_ROOM_LIMIT:
+                match = Match.objects.create()
+                room_key = RoomKey.create_room(room_type = "SIMPLE", table_id = match.id, match_id = match.id)
+                RoomMembers.objects.create(room_id = room_key, user = user)
+                return Response({"room_id": room_key}, status = status.HTTP_201_CREATED)
+            else:
+                return Response({"error": "Waiting room limit reached."}, status = status.HTTP_400_BAD_REQUEST)
+
+        except AuthError as e:
+            return Response({"error": str(e)}, status = status.HTTP_401_UNAUTHORIZED)
+        except DatabaseError as e:
+            return Response({"error": "Database error occurred."}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
