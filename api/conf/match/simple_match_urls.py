fe39927b470bf5a444961f4e6fd66428ea2af6de
sota <hotphilly1776@yahoo.co.jp>
2025-05-31 11:27:38 +0900

    Add join endpoint for simple match participation and implement match joining logic

--- api/conf/match/simple_match_urls.py
+++ api/conf/match/simple_match_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SimpleMatchView, SimpleMatchCreateView
+from .views import SimpleMatchView, SimpleMatchCreateView, SimpleMatchJoinView
 
 urlpatterns = [
     path('', SimpleMatchView.as_view(), name = 'simple-match'),
     path('create/', SimpleMatchCreateView.as_view(), name = 'simple-match-create'),
+    path('join/', SimpleMatchJoinView.as_view(), name = 'simple-match-join'),
 ]
--- api/conf/match/utils.py
+++ api/conf/match/utils.py
@@ -1,3 +1,4 @@
+from .models import Match, MatchDetail
 from room.models import RoomMembers
 from room.utils import RoomKey
 
@@ -16,3 +17,30 @@ def create_simple_match_response(waiting_simple_room_keys):
             'display_name': display_name
         })
     return response_data
+
+def try_join_match(match_id, user):
+    try:
+        match = Match.objects.get(id = match_id)
+        if match.is_finished:
+            return {"error": "Match is already finished."}, False
+        tournamant_id = match.tournament_id
+        if tournamant_id is None:
+            room_data = RoomKey.get_room(room_type = "SIMPLE", table_id = match_id)
+            room_id = RoomKey.generate_key(room_type = "SIMPLE", table_id = match_id)
+        else:
+            room_data = RoomKey.get_room(room_type = "TOURNAMENT_MATCH", table_id = match_id)
+            room_id = RoomKey.generate_key(room_type = "TOURNAMENT_MATCH", table_id = match_id, tournament_id = tournamant_id)
+        if not room_data:
+            return {"error": "Room does not exist."}, False
+        entry_count = RoomKey.increment_entry_count(room_type = room_data["type"], table_id = match_id)
+        if entry_count == -1:
+            return {"error": "Match is full."}, False
+        RoomMembers.objects.create(room_id = room_id, user = user)
+        room_members = RoomMembers.objects.filter(room_id = room_id)
+        if len(room_members) != 2:
+            return {"error": "RoomMembers count does not expected."}, False
+        for member in room_members:
+            MatchDetail.objects.create(match = match, user = member.user)
+        return {"match_id": match_id}, True     
+    except Match.DoesNotExist:
+        return {"error": "Match does not exist."}, False
