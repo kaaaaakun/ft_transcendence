91c9b7d4182c18d99e2bff0e1a15821329e6952e
sota <hotphilly1776@yahoo.co.jp>
2025-06-06 18:36:42 +0900

    Add MatchIdView endpoint to match URLs for retrieving match details by ID

--- api/conf/match/urls.py
+++ api/conf/match/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MatchViewSet, MatchDetailViewSet, LocalSimpleMatchView
+from .views import MatchViewSet, MatchDetailViewSet, LocalSimpleMatchView, MatchIdView
 
 router = DefaultRouter()
 router.register(r'match', MatchViewSet, basename='match')
@@ -8,5 +8,6 @@ router.register(r'detail', MatchDetailViewSet, basename='detail')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('local/', LocalSimpleMatchView.as_view(), name='local'),
+    path('local/', LocalSimpleMatchView.as_view(), name = 'local'),
+    path('<int:match_id>/', MatchIdView.as_view(), name = 'match_id'),
 ]
