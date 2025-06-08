be9955dce18126a0b3f7571ae2e854e108848723
sota <hotphilly1776@yahoo.co.jp>
2025-06-08 19:11:34 +0900

    リモートシンプル対戦で同じユーザーが同時に何部屋も作れてしまう Fixes #264

--- api/conf/match/views.py
+++ api/conf/match/views.py
@@ -6,14 +6,12 @@ from django.utils.decorators import method_decorator
 from django.db import DatabaseError
 
 from .models import Match, MatchDetail
-from .utils import create_simple_match_response, try_join_match
+from .utils import create_simple_match_response, try_join_match, try_create_simple_match
 from user.utils import get_user_by_auth
 from user.exceptions import AuthError
 from room.models import RoomMembers
 from room.utils import RoomKey
 from utils.decorators import admin_only
-
-SIMPLE_WAITING_ROOM_LIMIT = 5
 
 # start: ユースケースでは本来必要ないが、データの確認のために追加
 @method_decorator(admin_only, name = 'dispatch')
@@ -61,19 +59,12 @@ class SimpleMatchCreateView(APIView):
             user = get_user_by_auth(request.headers.get('Authorization'))
             if not user:
                 raise AuthError('Authorization header is incorrect.')
-            waiting_simple_room_keys = RoomKey.get_keys_by_type_and_entry_count("SIMPLE", 2)
-            if len(waiting_simple_room_keys) < SIMPLE_WAITING_ROOM_LIMIT:
-                match = Match.objects.create()
-                room_key = RoomKey.create_room(room_type = "SIMPLE", table_id = match.id, match_id = match.id)
-                RoomMembers.objects.create(room_id = room_key, user = user)
-                return Response({"match_id": match.id}, status = status.HTTP_201_CREATED)
-            else:
-                return Response({"error": "Waiting room limit reached."}, status = status.HTTP_400_BAD_REQUEST)
-
+            response, success = try_create_simple_match(user)
+            if not success:
+                return Response(response, status = status.HTTP_400_BAD_REQUEST)
+            return Response(response, status = status.HTTP_201_CREATED)
         except AuthError as e:
             return Response({"error": str(e)}, status = status.HTTP_401_UNAUTHORIZED)
-        except DatabaseError as e:
-            return Response({"error": "Database error occurred."}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class SimpleMatchJoinView(APIView):
     def post(self, request):
